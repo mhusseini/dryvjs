51578314c8b655cd73ac8fbbdce46408db3e8978
Munir Husseini <mhusseini@softewareproduction.eu>
2024-12-19 21:57:02 +0100

    again

--- packages/dryvjs/src/getDryvValidator.ts
+++ packages/dryvjs/src/getDryvValidator.ts
@@ -3,5 +3,5 @@ import { DryvValidator } from '@/DryvValidator'
 export function getDryvValidator<TModel extends object>(
   obj: any
 ): DryvValidator<TModel> | undefined {
-  return (obj as any)?.__dryvValidator ? (obj as DryvValidator<TModel>) : (obj as any).$validator
+  return (obj as any)?.__dryvValidator ? (obj as DryvValidator<TModel>) : (obj as any)?.$validator
 }
