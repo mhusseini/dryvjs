d7e9804de2e701390cc410d97ee229a19d1c0e39
Munir Husseini <mhusseini@softewareproduction.eu>
2026-06-09 02:54:40 +0200

    Refactored dryvjs

---
+++ packages/dryvjs/src/types/rules.ts
@@ -0,0 +1,53 @@
+import { DryvValidationSession } from '../session/DryvValidationSession'
+
+export type DryvValidateFunctionResult =
+    | DryvFieldValidationResult
+    | string
+    | boolean
+    | null
+    | undefined
+    | Promise<DryvFieldValidationResult | string | null | undefined>
+
+export interface DryvValidationRule<TModel extends object> {
+    async?: boolean
+    annotations?: {
+        required?: boolean
+        [path: string]: unknown
+    }
+    related?: string[]
+    group?: string
+    validate: <TInput = TModel>(
+        $m: TInput,
+        session: DryvValidationSession<TModel>
+    ) => DryvValidateFunctionResult
+}
+
+export type DryvRuleInvocations<TModel extends object> = {
+    [Property in keyof TModel]?: DryvValidationRule<TModel>[]
+} & {
+    [path: string]: DryvValidationRule<TModel>[]
+}
+
+export interface DryvValidationRuleSet<TModel extends object, TParameters = object> {
+    name: string
+    validators: DryvRuleInvocations<TModel>
+    disablers?: DryvRuleInvocations<TModel>
+    parameters?: TParameters
+}
+
+export interface DryvValidationRuleSetResolver {
+    name: string
+
+    resolve<TModel extends object, TParameters = object>(
+        ruleSetName: string
+    ): DryvValidationRuleSet<TModel, TParameters>
+}
+
+export interface DryvFieldValidationResult {
+    path?: string
+    type?: DryvValidationResultType
+    text?: string | null
+    group?: string | null
+}
+
+export type DryvValidationResultType = 'error' | 'warning' | 'success' | string
